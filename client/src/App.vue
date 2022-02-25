8e6d9e6ab167c57eba06357742448b4fce003e5a
Kajetan Domagala <kajtekd8@gmail.com>
2022-02-25 18:30:30 +0100

    Add footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,21 @@
   <div class="app">
     <Navbar />
     <Welcome />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Welcome from '@/components/Welcome.vue';
+import Footer from '@/components/Footer.vue';
 import Navbar from '@/components/Navbar.vue';
+import Welcome from '@/components/Welcome.vue';
 
 export default defineComponent({
   components: {
-    Welcome,
+    Footer,
     Navbar,
+    Welcome,
   },
 });
 </script>
---
+++ client/src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer>
+    <div class="max-w-7xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+      <nav>
+        <ul class="flex flex-wrap justify-center -mx-5 -my-2">
+          <li v-for="(link, index) in links" :key="index" class="px-5 py-2">
+            <a
+              :href="link.href"
+              class="text-base leading-6 transition-colors text-gray-500 hover:text-green-500"
+            >
+              {{ link.name }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p class="mt-8 text-base leading-6 text-center text-gray-400">
+        <a href="/"> cryptonow </a>
+        All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Link from '@/types/Link';
+
+export default defineComponent({
+  setup() {
+    const links = ref<Link[]>([
+      {
+        href: '#home',
+        name: 'Home',
+      },
+      {
+        href: '#transactions',
+        name: 'Transactions',
+      },
+      {
+        href: '#market',
+        name: 'Market',
+      },
+      {
+        href: '#news',
+        name: 'News',
+      },
+    ]);
+
+    return { links };
+  },
+});
+</script>
